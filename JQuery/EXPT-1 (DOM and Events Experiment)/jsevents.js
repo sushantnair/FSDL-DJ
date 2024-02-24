a91ed4f3416dcb1f2340f1815a2a106b7615279b
sushantnair <sushantnair.developer@gmail.com>
2024-02-24 16:16:15 +0530

    Create jsevents.js
    JS code to demonstrate JS events.

---
+++ JQuery/EXPT-1 (DOM and Events Experiment)/jsevents.js
@@ -0,0 +1,28 @@
+//Let's explore some events samples!
+
+var headOne = document.querySelector('#one')
+var headTwo = document.querySelector('#two')
+var headThree = document.querySelector('#three')
+
+// Hover (mouseover and mouseout)
+headOne.addEventListener('mouseover',function(){
+  headOne.textContent = "Mouse currently Over";
+  headOne.style.color = 'yellow';
+})
+
+headOne.addEventListener('mouseout',function(){
+  headOne.textContent = "Mouse Not On me."
+  headOne.style.color = 'blue';
+})
+
+// On Click
+headTwo.addEventListener("click",function(){
+  headTwo.textContent = "Clicked On";
+  headTwo.style.color = 'green';
+})
+
+// Double Click
+headThree.addEventListener("dblclick",function(){
+  headThree.textContent = "Double Clicked!";
+  headThree.style.color = 'cyan';
+})
