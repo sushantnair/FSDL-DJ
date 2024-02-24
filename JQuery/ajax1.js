b2f4f96ed756ca3bf67ebaab301a20f984d59059
sushantnair <sushantnair.developer@gmail.com>
2024-02-24 16:38:49 +0530

    Create ajax1.js

---
+++ JQuery/ajax1.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("#code").load("jquery.js");
+    $("#jquerycode").load("jquery.js");
+    $(document).ready(function() {
+        $("#jquerycode").load("jquery.js");
+    });
+});
