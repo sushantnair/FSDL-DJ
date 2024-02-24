78dc8e465600453c0acbd853fb6d07acc9080dc5
sushantnair <sushantnair.developer@gmail.com>
2024-02-24 16:16:43 +0530

    Create selector.js
    Demonstration of selection of elements using JS.

---
+++ JQuery/EXPT-1 (DOM and Events Experiment)/selector.js
@@ -0,0 +1,27 @@
+$(function () {
+    //$("h1").css("background-color", "red");
+
+    // Select all elements with the class .red-box (only one in this case)
+    //$(".green-box").css("background-color", "rgba(180, 180, 30, 0.8)");
+   
+    // Select the unique element with ID "list"
+    //$("#list").css("background-color", "rgba(180, 180, 30, 0.8)");
+ 
+    // Select all h2, p, and input tags
+    //$("h2, p, input").css("background-color", "rgba(180, 180, 30, 0.8)");
+ 
+    // Select only the last <li> on the page
+    //$("li:last").css("background-color", "rgba(180, 180, 30, 0.8)");
+ 
+    // Select all even list items on the page (every second list item)
+    //$("li:even").css("background-color", "rgba(180, 180, 30, 0.8)");
+   
+    // Select all odd list items (every other list item)
+    //$("li:odd").css("background-color", "rgba(180, 180, 30, 0.8)");
+   
+    // Select all input elements that are checked/selected
+    // Checkboxes and radio buttons don't have background-color
+    //$("toc:checked").css("background-color", "rgba(180, 180, 30, 0.8)");
+
+    //$("input[type=email]").css("background-color", "rgba(180, 180, 30, 0.8)");
+});
