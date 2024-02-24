24963e5ff13dbecdc45c91d3b9775e6633f7446c
sushantnair <sushantnair.developer@gmail.com>
2024-02-24 16:13:34 +0530

    Create filter.js
    Filters

---
+++ JQuery/EXPT-1 (DOM and Events Experiment)/filter.js
@@ -0,0 +1,13 @@
+$(function(){
+    //$("#list").children("li").filter(":odd").css("background-color", "rgba(180, 180, 30, 0.8)")
+
+    //$("li").first().css("background-color", "rgba(180, 180, 30, 0.8)")
+
+    //$("li").last().css("background-color", "rgba(180, 180, 30, 0.8)")
+
+    //$("li").eq(4).css("background-color", "rgba(180, 180, 30, 0.8)")
+
+    //$("li").eq(-2).css("background-color", "rgba(180, 180, 30, 0.8)")
+
+    //$("li").not(":first").css("background-color", "rgba(180, 180, 30, 0.8)")
+})
